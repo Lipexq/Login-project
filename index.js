e32fcce038acb42b1f6723b89b9a3033573c93ef
Filipe Alexssandro <18fiale@gmail.com>
2023-06-15 05:23:26 -0300

    front-end adjuste

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+/* Focus nos imputs efeitos */
+
 let inputs = document.getElementsByClassName("input-form");
 
-for( let input of inputs) {
-    input.addEventListener("blur", function(){
-        if(input.value.trim() != ""){
+for (let input of inputs) {
+    input.addEventListener("blur", function () {
+        if (input.value.trim() != "") {
             input.classList.add("has-val");
-        } else{
+        } else {
             input.classList.remove("has-val");
         }
     })
-}
+};
+
+// Verificar o Email e desabilitar o botão de enviar 
+
+var inputEmail = document.getElementById("email")
+
+inputEmail.addEventListener("blur", function () {
+    let valor = inputEmail.value.trim();
+    let changeColor = document.getElementsByClassName("focus-input-form")[0];
+    let text = document.getElementsByClassName("texthidden")[0];
+    let btn = document.getElementById("login-bnt");
+
+    if (valor.includes("@") && valor.includes(".com")) {
+        changeColor.classList.remove("error");
+        text.classList.remove("visible");
+        btn.classList.add("enable");
+
+    } else {
+        changeColor.classList.add("error");
+        text.classList.add("visible");
+        btn.classList.add("block");
+    }
+    
+});
